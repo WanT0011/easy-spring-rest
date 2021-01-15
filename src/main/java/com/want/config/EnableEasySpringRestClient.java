64bc8fb81605dec949d7eb3b14304ac7004b8e0d
wangzhijian <wangzhijian01@xkw.cn>
2021-01-15 09:45:51 +0800

    更换为WebClient.ResponseSpec

--- src/main/java/com/want/config/EnableEasySpringRestClient.java
+++ src/main/java/com/want/config/EnableEasySpringRestClient.java
@@ -1,7 +1,6 @@
 package com.want.config;
 
 import com.want.EasySpringRestAutoConfiguration;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
@@ -10,7 +9,7 @@ import java.lang.annotation.*;
  * @author want
  * @createTime 2021.01.11.21:29
  */
-@Target({ ElementType.FIELD})
+@Target({ ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
