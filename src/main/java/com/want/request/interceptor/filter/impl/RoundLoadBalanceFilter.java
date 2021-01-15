5cc87afd75a597a95b2850614fdab782aec23798
wangzhijian <wangzhijian01@xkw.cn>
2021-01-15 09:35:56 +0800

    更换为WebClient.ResponseSpec

--- src/main/java/com/want/request/interceptor/filter/impl/RoundLoadBalanceFilter.java
+++ src/main/java/com/want/request/interceptor/filter/impl/RoundLoadBalanceFilter.java
@@ -1,12 +1,14 @@
 package com.want.request.interceptor.filter.impl;
 
 import com.want.request.interceptor.filter.AbstractLoadBalanceExecutorFilter;
+import com.want.request.loadbalance.EasySpringRestLoadBalance;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.util.CollectionUtils;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -18,10 +20,14 @@ public class RoundLoadBalanceFilter extends AbstractLoadBalanceExecutorFilter {
 
     private DiscoveryClient discoveryClient;
 
+    private EasySpringRestLoadBalance easySpringRestLoadBalance;
+
+
     private AtomicInteger curCount = new AtomicInteger(0);
 
-    public RoundLoadBalanceFilter(DiscoveryClient discoveryClient) {
+    public RoundLoadBalanceFilter(DiscoveryClient discoveryClient,EasySpringRestLoadBalance easySpringRestLoadBalance) {
         this.discoveryClient = discoveryClient;
+        this.easySpringRestLoadBalance = easySpringRestLoadBalance;
     }
 
     @Override
@@ -29,19 +35,20 @@ public class RoundLoadBalanceFilter extends AbstractLoadBalanceExecutorFilter {
         String host = uri.getHost();
 
         List<ServiceInstance> instances = discoveryClient.getInstances(host);
-
         if(CollectionUtils.isEmpty(instances)){
             throw new RuntimeException(String.format("%s 在注册中心无可用实例！",host));
         }
-        ServiceInstance instance;
-        if(instances.size() == 1){
-            instance = instances.get(0);
-        }else{
-            ServiceInstance[] serviceInstances = instances.toArray(new ServiceInstance[0]);
-            int curIndex = curCount.incrementAndGet();
-            instance = serviceInstances[curIndex%serviceInstances.length];
-        }
 
+        ServiceInstance instance = easySpringRestLoadBalance.doSelect(easySpringRestLoadBalance, instances, Collections.EMPTY_LIST);
+
+//        ServiceInstance instance;
+//        if(instances.size() == 1){
+//            instance = instances.get(0);
+//        }else{
+//            ServiceInstance[] serviceInstances = instances.toArray(new ServiceInstance[0]);
+//            int curIndex = curCount.incrementAndGet();
+//            instance = serviceInstances[curIndex%serviceInstances.length];
+//        }
         String oldPath = uri.toString();
         String suffix = oldPath.substring(0, oldPath.indexOf(host) + host.length());
         try {
